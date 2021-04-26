1e79b951c3d286d19db9cfced15ba5648c33c498
AhsanRiyad <riyad298@gmail.com>
2021-04-27 05:30:01 +0600

    tawilwind is working

---
+++ components/layout/footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const abc: React.FC = ()=>{
+    return (<div>
+        this is footer
+    </div>);
+}
+
+export default abc;
---
+++ components/layout/header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const abc: React.FC = () => {
+    return (<div>
+       
+        <div className="grid grid-cols-2 bg-red-50">
+            <div>
+                one
+            </div>
+            <div>
+                two
+            </div>
+        </div>
+
+    </div>);
+}
+
+export default abc;
