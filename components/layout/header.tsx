2e94532086bd748ae914f758a23ba19387475c4e
AhsanRiyad <riyad298@gmail.com>
2021-04-28 04:12:45 +0600

    home page design

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 
 const abc: React.FC = () => {
     return (<div>
-       
-        <div className="grid grid-cols-2 bg-red-50">
-            <div>
-                one
+        <div className="flex h-12 text-white bg-gradient-to-r from-red-500 to-indigo-500  justify-center  items-center">
+            <span className="inline cursor-pointer">SALE Unlimited Downloads! </span> <span className="inline cursor-pointer"> &nbsp; 40% Off on all elements </span>
+       </div>
+        <div className="grid grid-cols-12 bg-gray-700 text-white h-12 place-content-center">
+            <div className="col-start-2 self-center cursor-pointer">
+                MarketPlace
             </div>
-            <div>
-                two
+            <div className="col-start-8  col-span-4">
+                <div className="grid justify-end">
+                    <div> <button className="bg-gray-500 py-3 px-4 cursor-pointer"> Signin </button> </div>
+                </div>
+            </div>
+        </div>
+        <div className="grid grid-cols-6 bg-gray-700 text-white h-12 place-content-center">
+                <div className="col-start-2 col-span-4 relative">
+                    <div className="inline mx-3 cursor-pointer relative"> Web Theme & Template 
+                        <div className="absolute inset-x-1/2 -bottom-8"><ArrowDropUpIcon style={{ fontSize: 37 }}/></div>
+                    </div>
+                    <div className="inline mx-3 cursor-pointer"> Code </div>
+                    <div className="inline mx-3 cursor-pointer"> Graphics </div>
+                    <div className="inline mx-3 cursor-pointer"> Photo </div>
+                    <div className="absolute right-4 -bottom-10 rounded bg-gray-500 py-4 px-4">MarketPlace</div>
+                </div>
+        </div>
+
+        <div className=" grid grid-cols-6 border text-black h-14 place-content-center">
+            <div className="col-start-2 col-span-4 relative text-xs ">
+                <div className="inline mx-3 cursor-pointer"> All items </div>
+                <div className="inline mx-3 cursor-pointer"> Wordpress </div>
+                <div className="inline mx-3 cursor-pointer"> Template </div>
+                <div className="inline mx-3 cursor-pointer"> CMS </div>
             </div>
         </div>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Footer from '../components/layout/footer';
 import Header from '../components/layout/header';
-
+import SearchIcon from '@material-ui/icons/Search';
 
 export default function Home() {
   return (
@@ -12,16 +12,33 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>
       <Header/>
+      <div className="bg-blue-50">
+        <div className="grid grid-cols-6  text-black  place-content-center">
+          <div className="col-start-2 col-span-2 mt-32">
+            <div className="font-bold text-3xl">
+              51,528 WordPress Themes & Website Templates From $2 
+            </div>
+            <div  className="mt-6">
+              WordPress themes, web templates and more. Brought to you by the largest global community of creatives.
+            </div>
+            <div>
+              <div className="mt-10	grid bg-white shadow-2xl px-2 rounded border " style={{ gridTemplateColumns: " 1fr 100px "  }}>
+                    <input placeholder="e.g Responsive web design" className=" py-6 bg-white focus:outline-none mx-3"/>
+                    <button className="bg-green-700 text-white px-3 rounded-lg my-3"> 
+                      <SearchIcon/> 
+                      Search
+                    </button>
+              </div>
+            </div>
+          </div>
 
+          <div className="col-span-2 place-self-center">
+            <img src="/static/search-header-1600749088571.webp"  className="mx-3"/>
+          </div>
+
+        </div>
       </div>
-      <div>
-        this is index...
-      </div>
-
-      <Footer/>
-
     </div>
   )
 }
